df830ced81170182f66514e8b08fc0f2e004c6b8
Aurelius <89624318+Aurelius77@users.noreply.github.com>
2023-03-14 01:36:01 +0100

    Add files via upload

--- open_ai.py
+++ open_ai.py
@@ -1,61 +1,81 @@
 import openai
+import tkinter as tk
 import customtkinter as ct
 import json
 
 
-def getSummary():
-    text = entry.get()
-    openai.api_key = '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'
-    response = openai.Completion.create(
-        model='text-davinci-003',
-        prompt=f'Summarize this text with the most unique and helpful points\n\n{text}',
-        temperature=0.7,
-        max_tokens=1000,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
-    # text_var.set(str(response))
-    json_data = str(response)
-    data_dict = json.loads(json_data)
-    sum_text = (data_dict['choices'][0]['text'])
-    with open("summary.txt", "w") as file:
-        file.write(sum_text)
-    label_var.set('Text file with summary succesfully created')
+class App(ct.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.geometry('600x600')
+        self.title('Text Summarizer')
+        ct.set_appearance_mode('system')
+        ct.set_default_color_theme('green')
 
 
-root = ct.CTk()
-root.geometry('400x400')
-root.title('Text Summarizer')
-ct.set_appearance_mode('system')
-# ct.set_default_color_theme('dark-blue')
+        #INPUT BOX
+        self.entry = ct.StringVar()
 
 
-entry = ct.StringVar()
-emtry_space = ct.CTkEntry(master=root,
-                          textvariable=entry,
+        self.entry_space = ct.CTkEntry(master=self,
+                          textvariable=self.entry,
                           width=200,
-                          height=20,
+                          height=100,
                           border_width=2,
-                          corner_radius=10,
                           placeholder_text='Enter text...',
                           text_color='white'
                           ).pack(padx=40, pady=20)
+        
+        #SUBMIT BUTTON
+        
+        self.button = ct.CTkButton(
+            master=self, text='GET SUMMARY', command=self.getSummary)
 
 
-button = ct.CTkButton(master=root, text='GET SUMMARY', command=getSummary)
-button.pack(padx=20, pady=10)
+        self.button.pack(padx=20, pady=10)
 
 
-label_var = ct.StringVar(value='')
+        #SUMMARIZED TEXT
 
-label = ct.CTkLabel(master=root,
-                    textvariable=label_var,
+        self.label_var = ct.StringVar(value='')
+
+
+        self.label = ct.CTkLabel(master=self,
+                    textvariable=self.label_var,
                     width=120,
                     height=25,
                     fg_color=("white", "gray20"),
+                    justify='center',
+                    anchor=tk.CENTER,
                     corner_radius=8)
-label.pack(padx=20, pady=10)
+        self.label.pack(padx=20, pady=10)
 
 
-root.mainloop()
+  #API CALL UNCTION
+    def getSummary(self):
+     
+        text = self.entry.get()
+        openai.api_key = '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'
+        response = openai.Completion.create(
+            model='text-davinci-003',
+            prompt=f'Summarize this text with the most unique and helpful parts\n\n{text}',
+            temperature=0.7,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+       # text_var.set(str(response))
+        json_data = str(response)
+        data_dict = json.loads(json_data)
+        sum_text = (data_dict['choices'][0]['text'])
+        with open("summary.txt", "w") as file:
+            file.write(sum_text)
+        self.label_var.set('Text file with summary succesfully created')
+    
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
